1351e518757682c7342b95bd0c188008706ffa3a
Vlad845 <80671666+Vlad845@users.noreply.github.com>
2022-10-29 20:49:41 +0300

    Made the Home Page Responsive

--- frontend/wine-and-dine/src/pages/homepage/HomePage.js
+++ frontend/wine-and-dine/src/pages/homepage/HomePage.js
@@ -1,10 +1,25 @@
 import React from "react";
 import "./HomePage.css" 
-
+import Logo from "../../photos/logo.png"
 
 const HomePage= ()=>{
     return(
-        <div>home</div>
+        <div className="homepage">
+           
+           <div className="wraper">
+
+                <div className="up">
+                    <h1 className="companyname">Wine&Dine</h1> 
+                    <img className="companyphoto"src={Logo}></img>
+                </div>
+
+                <div className="down">
+                    <button className="loginbutton">Login</button>
+                    <h1 className="descriptionhomepage">Don't have an account yet?</h1>
+                    <button className="registerbutton">Sign Up</button>
+                </div>
+           </div>
+        </div>
     )
 }
 
