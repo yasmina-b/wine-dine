22b3b9806d2f30c10947ad9f304551f7c210ce24
Vlad845 <80671666+Vlad845@users.noreply.github.com>
2022-11-08 13:05:04 +0200

    Made the Register Page Responsive

--- frontend/wine-and-dine/src/pages/registerpage/RegisterPage.js
+++ frontend/wine-and-dine/src/pages/registerpage/RegisterPage.js
@@ -35,41 +35,54 @@ function RegisterPage() {
       <div className="title">SIGN UP</div>
       <form>
         <label className="labels">First Name</label>
+        <div className="input-wrapper">
         <input
           className="input-style"
           type="text"
           value={firstName}
           onChange={(e) => setFirstName(e.target.value)}
         />
+        </div>
+        
         <label className="labels">Last Name</label>
+        <div className="input-wrapper">
         <input
           className="input-style"
           type="text"
           value={lastName}
           onChange={(e) => setLastName(e.target.value)}
         />
+        </div>
         <label className="labels">Email</label>
+        <div className="input-wrapper">
         <input
           className="input-style"
           type="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
+        </div>
         <label className="labels">Phone Number</label>
+        <div className="input-wrapper">
         <input
           className="input-style"
           type="text"
           value={phoneNumber}
           onChange={(e) => setPhoneNumber(e.target.value)}
         />
+        </div>
         <label className="labels">Password</label>
+        <div className="input-wrapper">
         <input
           className="input-style"
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
+        </div>
+        <div className="button-wrapper">
         <button className="button" onClick={(e) => handleRegister(e)}>Sign up</button>
+        </div>
       </form>
     </div>
   );
