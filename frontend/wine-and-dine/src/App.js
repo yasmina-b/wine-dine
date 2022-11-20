ff9bc5cf11f38efc151f59824bafec166bfaff98
Yasmina Bocereg <yasminabocereg@Yasminas-MacBook-Pro.local>
2022-11-20 21:13:26 +0200

    added AuthContext

--- frontend/wine-and-dine/src/App.js
+++ frontend/wine-and-dine/src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import AppRoutes from './AppRoutes';
+import React from "react";
+import AppRoutes from "./AppRoutes";
+import AuthContext from "./context/AuthContext";
 
 function App() {
+  const [user, setUser] = React.useState(null);
+
+  React.useEffect(() => {
+    const loggedInUser = localStorage.getItem("user");
+    if (loggedInUser) {
+      const foundUser = JSON.parse(loggedInUser);
+      setUser(foundUser);
+    }
+  }, []);
+
   return (
-    <AppRoutes/>
+    <AuthContext.Provider value={{ user, setUser }}>
+      <AppRoutes />;
+    </AuthContext.Provider>
   );
 }
 
