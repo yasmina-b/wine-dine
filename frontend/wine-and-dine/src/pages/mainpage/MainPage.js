42ed77e2d81a27971994064b5287a0b1f59933f3
Vlad845 <80671666+Vlad845@users.noreply.github.com>
2022-11-29 00:44:04 +0200

    Worked on MainPage

--- frontend/wine-and-dine/src/pages/mainpage/MainPage.js
+++ frontend/wine-and-dine/src/pages/mainpage/MainPage.js
@@ -3,9 +3,10 @@ import "./MainPage.css";
 import Navbar from "../../components/navbar/Navbar";
 import MenuBar from "../../components/menubar/MenuBar";
 
+
 const MainPage = () => {
   return (
-    <div class="main-page">
+    <div className="main-page">
       <link
         rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
@@ -14,7 +15,9 @@ const MainPage = () => {
       <div className="alignment">
         <div className="inline">
           <div className="input-icons">
-            <span class="material-symbols-outlined">search</span>
+            <div className="lupa">
+            <img src="../../photos/search-icon.png" ></img>
+            </div>
             <input
               className="input-style"
               placeholder="Search your favorite restaurant..."
