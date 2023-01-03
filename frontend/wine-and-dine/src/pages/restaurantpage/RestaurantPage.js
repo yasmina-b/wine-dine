580352314c83d4add7703300ff771736d910698c
Yasmina Bocereg <yasminabocereg@Yasminas-MacBook-Pro.local>
2023-01-04 00:55:28 +0200

    wishlist done + update user account info

--- frontend/wine-and-dine/src/pages/restaurantpage/RestaurantPage.js
+++ frontend/wine-and-dine/src/pages/restaurantpage/RestaurantPage.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import Navbar from "../../components/navbar/Navbar";
 import "./RestaurantPage.css";
-import MenuBar from "../../components/menubar/MenuBar";
+import AuthContext from "../../context/AuthContext";
 
 const RestaurantPage = () => {
-  const { id } = useParams();
+  const {id} = useParams();
   const [restaurant, setRestaurant] = useState({});
+  const {wishlist, customSetWishlist} = React.useContext(AuthContext);
 
   const getRestaurantById = async () => {
     try {
@@ -15,7 +15,6 @@ const RestaurantPage = () => {
         `http://localhost:8800/api/restaurants/${id}`
       );
       setRestaurant(response.data);
-      console.log(response.data);
     } catch (err) {
       console.error(err);
     }
@@ -27,40 +26,42 @@ const RestaurantPage = () => {
 
   if (!restaurant) return <h1>Loading...</h1>;
 
-  
+  const handleAddRestaurant = (restaurant) => {
+    customSetWishlist([...wishlist, restaurant]);
+  }
+
   return (
     <div className="main-page">
       <div className="imageRestaurantPageWrapper">
-     <img className="imageRestaurantPage"src={restaurant.image}></img>
-     </div>
-
-      <div>
-      <button type="button" className="favoriteButton"></button>
+        <img className="imageRestaurantPage" src={restaurant.image} alt=""></img>
       </div>
 
       <div>
-      <button type="button" className="backButton"></button>
+        <button type="button" className="favoriteButton" onClick={() => handleAddRestaurant(restaurant)}></button>
+      </div>
+
+      <div>
+        <button type="button" className="backButton"></button>
       </div>
 
       <div className="nameRestaurantPage">
-      <h3>{restaurant.name}</h3>
+        <h3>{restaurant.name}</h3>
       </div>
 
       <div className="categoryRestaurantPage">
-      <h2>{restaurant.category}</h2>
-
+        <h2>{restaurant.category}</h2>
       </div>
 
       <div className="scheduleCostRestaurantPage">
-      <h2>Open: {restaurant.schedule} </h2>
+        <h2>Open: {restaurant.schedule} </h2>
       </div>
 
       <div className="ratingRestaurantPage">
-      <h2>Rating: {restaurant.rating}</h2>
+        <h2>Rating: {restaurant.rating}</h2>
       </div>
 
       <div className="averageCostRestaurantPage">
-      <h2>Average Cost: {restaurant.averageCost} lei for 2 people</h2>
+        <h2>Average Cost: {restaurant.averageCost} $ for 2 people</h2>
       </div>
 
       <div className="ButtonWrapper">
@@ -70,18 +71,15 @@ const RestaurantPage = () => {
       </div>
 
       <div className="TextWrapper">
-        <h2  className="websiteText">View website</h2>
-        <h2  className="websiteText">Menu</h2>
-        <h2  className="websiteText">Go to Maps</h2>
+        <h2 className="websiteText">View website</h2>
+        <h2 className="websiteText">Menu</h2>
+        <h2 className="websiteText">Go to Maps</h2>
       </div>
-     
-      
+
       <div className="bookButtonWrapper">
-      <button className="bookButton">BOOK A TABLE</button>
+        <button className="bookButton">BOOK A TABLE</button>
       </div>
       {/* <div className="poitionMenuBar"><MenuBar></MenuBar></div> */}
-      
-
     </div>
   );
 };
--- frontend/wine-and-dine/src/pages/wishlistpage/WishlistPage.js
+++ frontend/wine-and-dine/src/pages/wishlistpage/WishlistPage.js
@@ -1,13 +1,34 @@
 import React from "react";
 import MenuBar from "../../components/menubar/MenuBar";
 import Navbar from "../../components/navbar/Navbar";
+import AuthContext from "../../context/AuthContext";
 import "./WishlistPage.css";
 
 const WishlistPage = () => {
+  const { wishlist, customSetWishlist } = React.useContext(AuthContext);
+
+  const removeItemFromWishlist = (index) => {
+    const newWishlist = [...wishlist];
+    newWishlist.splice(index, 1);
+    customSetWishlist(newWishlist);
+  };
+
   return (
     <div className="main-page">
       <Navbar></Navbar>
       <h1>MY WISHLIST</h1>
+      <div>
+        {wishlist.map((restaurant, index) => {
+          return (
+            <div>
+              <div>{restaurant.name}</div>
+              <button onClick={() => removeItemFromWishlist(index)}>
+                DELETE
+              </button>
+            </div>
+          );
+        })}
+      </div>
       <MenuBar></MenuBar>
     </div>
   );
