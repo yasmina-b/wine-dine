b291c35a57cf9af7f75c0a510b1241355fe664ea
Vlad845 <80671666+Vlad845@users.noreply.github.com>
2023-01-17 20:16:40 +0200

    Worked

--- frontend/wine-and-dine/src/pages/loginpage/LogInPage.js
+++ frontend/wine-and-dine/src/pages/loginpage/LogInPage.js
@@ -5,29 +5,34 @@ import { useNavigate } from "react-router-dom";
 import "./LoginPage.css";
 import AuthContext from "../../context/AuthContext";
 import Navbar from "../../components/navbar/Navbar";
+import Alert from "@mui/material/Alert";
 
 function LogInPage() {
   const navigate = useNavigate();
-
   const { setUser } = React.useContext(AuthContext);
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
 
   const handleLogin = async (e) => {
     e.preventDefault();
 
-    const res = await axios.post("http://localhost:8800/api/login", {
-      email,
-      password,
-    });
+    try {
+      const res = await axios.post("http://localhost:8800/api/login", {
+        email,
+        password,
+      });
 
-    setUser(res.data);
-    localStorage.setItem("user", JSON.stringify(res.data));
+      setUser(res.data);
+      localStorage.setItem("user", JSON.stringify(res.data));
 
-    if (res.status === 200) {
-      console.log("login successful");
-      navigate("/restaurants");
+      if (res.status === 200) {
+        setMessage("Successfully logged in!");
+        console.log("login successful");
+        navigate("/restaurants");
+      }
+    } catch (error) {
+      setMessage("Error: Login failed");
     }
   };
   return (
@@ -60,6 +65,21 @@ function LogInPage() {
         <button className="button-login" onClick={(e) => handleLogin(e)}>
           LOGIN
         </button>
+      </div>
+      <div className="messageWrapper">
+        {message ? (
+          <Alert
+            sx={{
+              bottom:"200px",
+              width: "550px",
+              fontSize: "60px",
+              display: "flex",
+              alignItems: "center",
+            }}
+            severity="error">
+            {message}
+          </Alert>
+        ) : null}
       </div>
     </body>
   );
